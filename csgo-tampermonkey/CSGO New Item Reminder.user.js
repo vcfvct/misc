9993b5e3d528e135deeda717233a95ab5c88a380
Han Li <Han.Li@capitalone.com>
2017-08-08 01:20:57 -0400

    cs go market reminder

---
+++ csgo-tampermonkey/CSGO New Item Reminder.user.js
@@ -0,0 +1,94 @@
+// ==UserScript==
+// @name         CSGO New Item Reminder
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Auto Refresh page if no item is changed in the page. When new item appears, there will be Desktop notification as well as info banner for reminding.
+// @author       Han Li
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.6.15/browser-polyfill.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.6.15/browser.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js
+// @include      https://steamcommunity.com/market/listings*
+// @grant GM_addStyle
+// @grant GM_notification
+// ==/UserScript==
+
+/* jshint ignore:start */
+var inline_src = (<><![CDATA[
+/* jshint ignore:end */
+/* jshint esnext: true */
+
+// Your code here...
+
+	GM_addStyle('#refresh-button { position: fixed; right: 3px; bottom: 50%; height: 40px; width: 80px;  background-color: #55E; color: #FFF; border-radius: 6px;}');
+	GM_addStyle('#info-banner { top: 0; height: 40px; width: 100%;  background-color: yellow; color: black; border-radius: 6px; font-size: 2em!important; text-align:center;}');
+
+	let pageTitle = $(document).find("title").text();
+	let itemNumber = parseInt($('#searchResults_total').text());
+	let previousItemNumber = parseInt(sessionStorage.getItem(pageTitle));
+
+	const refreshKey = pageTitle + '-refresh';
+	let refreshing = JSON.parse(sessionStorage.getItem(refreshKey));
+	console.log(`page loaded: prev: ${previousItemNumber} current: ${itemNumber}. time: ${new Date()}`);
+
+	if (isNaN(previousItemNumber)) {
+	    sessionStorage.setItem(pageTitle, itemNumber);
+	} else {
+	    if (refreshing && itemNumber > previousItemNumber) {
+	        refreshing = false;
+	        sessionStorage.setItem(refreshKey, refreshing);
+	        const msg = `Item increased from ${previousItemNumber} to ${itemNumber}. `;
+	        var notificationDetails = {
+	            text: msg,
+	            title: 'Refresh Stopped, you can manual refresh again by pressing the button.',
+	            timeout: 10000,
+	            onclick: function() {
+	                console.log("Notice clicked.");
+	                window.focus();
+	            }
+	        };
+	        GM_notification(notificationDetails);
+	        let infoBanner = $(`<div id='info-banner'><h1>${msg}</h1></div>`);
+	        $('body').prepend(infoBanner);
+	    }
+	}
+
+
+	let buttonText = getNextButtonState(refreshing);
+	console.log(refreshing);
+	let refreshButton = $(`<input type="button" id="refresh-button" value="${buttonText}"/>`);
+	console.log(refreshButton);
+
+	refreshButton.on('click', () => {
+	    refreshing = !refreshing;
+	    refreshButton.prop('value', getNextButtonState(refreshing));
+	    sessionStorage.setItem(refreshKey, refreshing);
+	    if (refreshing) {
+	        location.reload();
+	    }
+	    console.log(`after button click: ${refreshing}`);
+
+	});
+
+	if (refreshing) {
+	    setTimeout(() => {
+	            let shouldRefresh = JSON.parse(sessionStorage.getItem(refreshKey));
+	            console.log('in setTimeout: ' + shouldRefresh);
+	            if (shouldRefresh) {
+	                location.reload();
+	            }
+	        },
+	        31 * 1000
+	    );
+	}
+
+	$('body').append(refreshButton);
+
+	function getNextButtonState(refreshing) {
+	    return refreshing ? 'Stop Refresh' : 'Start Refresh';
+	}
+
+/* jshint ignore:start */
+]]></>).toString();
+var c = babel.transform(inline_src);
+eval(c.code);
+/* jshint ignore:end */
