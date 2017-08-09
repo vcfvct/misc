e967afd0521a8bdef84c08e8df50da58db6eb01f
Han Li <vcfvct@gmail.com>
2017-08-08 20:40:20 -0400

    several bug fixes: 1. item count not set to session storage after change detected. 2.number with comma from webpage is not correctly parsed 3.make the top banner fixed. 4. adjust the logging

--- csgo-tampermonkey/CSGO New Item Reminder.user.js
+++ csgo-tampermonkey/CSGO New Item Reminder.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         CSGO New Item Reminder
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Auto Refresh page if no item is changed in the page. When new item appears, there will be Desktop notification as well as info banner for reminding.
 // @author       Han Li
 // @require      https://cdnjs.cloudflare.com/ajax/libs/babel-core/5.6.15/browser-polyfill.min.js
@@ -20,15 +20,15 @@ var inline_src = (<><![CDATA[
 // Your code here...
 
 	GM_addStyle('#refresh-button { position: fixed; right: 3px; bottom: 50%; height: 40px; width: 80px;  background-color: #55E; color: #FFF; border-radius: 6px;}');
-	GM_addStyle('#info-banner { top: 0; height: 40px; width: 100%;  background-color: yellow; color: black; border-radius: 6px; font-size: 2em!important; text-align:center;}');
+	GM_addStyle('#info-banner {position: fixed; top: 0; height: 40px; width: 100%;  background-color: yellow; color: black; border-radius: 6px; font-size: 2em!important; text-align:center; z-index: 403;}');
 
 	let pageTitle = $(document).find("title").text();
-	let itemNumber = parseInt($('#searchResults_total').text());
+	let itemNumber = parseInt($('#searchResults_total').text().replace(/,/g,''));
 	let previousItemNumber = parseInt(sessionStorage.getItem(pageTitle));
 
 	const refreshKey = pageTitle + '-refresh';
 	let refreshing = JSON.parse(sessionStorage.getItem(refreshKey));
-	console.log(`page loaded: prev: ${previousItemNumber} current: ${itemNumber}. time: ${new Date()}`);
+	console.log(`page loaded: prev: '${previousItemNumber}' current: '${itemNumber}'. time: ${new Date()}`);
 
 	if (isNaN(previousItemNumber)) {
 	    sessionStorage.setItem(pageTitle, itemNumber);
@@ -36,10 +36,11 @@ var inline_src = (<><![CDATA[
 	    if (refreshing && itemNumber > previousItemNumber) {
 	        refreshing = false;
 	        sessionStorage.setItem(refreshKey, refreshing);
+	        sessionStorage.setItem(pageTitle, itemNumber);
 	        const msg = `Item increased from ${previousItemNumber} to ${itemNumber}. `;
 	        var notificationDetails = {
 	            text: msg,
-	            title: 'Refresh Stopped, you can manual refresh again by pressing the button.',
+	            title: 'Refresh Stopped, press the Button to resume refresh',
 	            timeout: 10000,
 	            onclick: function() {
 	                console.log("Notice clicked.");
@@ -52,11 +53,8 @@ var inline_src = (<><![CDATA[
 	    }
 	}
 
-
 	let buttonText = getNextButtonState(refreshing);
-	console.log(refreshing);
 	let refreshButton = $(`<input type="button" id="refresh-button" value="${buttonText}"/>`);
-	console.log(refreshButton);
 
 	refreshButton.on('click', () => {
 	    refreshing = !refreshing;
