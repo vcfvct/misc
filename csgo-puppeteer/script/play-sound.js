dc657d7e71684021d3faf1920eb2bf5cf5b0df3f
HanLi <vcfvct@gmail.com>
2017-10-09 00:14:16 -0400

    add a test for $$

--- csgo-puppeteer/script/play-sound.js
+++ csgo-puppeteer/script/play-sound.js
@@ -1,7 +1,6 @@
 #!/usr/bin/env node
 const play = require('../src/sound');
 const config = require('../src/config');
-const player = require('play-sound')(opts = {});
 
 play(config.soundFilePath);
 
--- csgo-puppeteer/test/puppeteer-el-test.js
+++ csgo-puppeteer/test/puppeteer-el-test.js
@@ -11,3 +11,22 @@ const url = 'https://steamcommunity.com/market/listings/730/%E2%98%85%20M9%20Bay
     console.log(`From 'page.evaluate' ${count}, From 'page.$eval' ${count1}`);
     browser.close();
 })();
+
+(async () => {
+    const fastUrl = 'https://varvy.com/pagespeed/wicked-fast.html';
+    const browser = await puppeteer.launch({ headless: false, delay: 3000 });
+    const page = await browser.newPage();
+    await page.goto(fastUrl);
+    const links = await page.$$('a');
+    console.log(`find ${links.length} links`);
+    for (let link of links) {
+        const linkText = await page.evaluate(el => el.innerHTML, link);
+        console.info(`evaluating text: ${linkText}`);
+        if (linkText === 'defer it') {
+            console.log(`find link: ${link}`);
+            await link.click();
+            break;
+        }
+    }
+    browser.close();
+})();
