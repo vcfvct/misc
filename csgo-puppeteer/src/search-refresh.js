63c765d36864248abcd4660ba13ac2dca4c079aa
HanLi <vcfvct@gmail.com>
2017-11-01 22:48:58 -0400

    add keyword filter for search refresh

--- csgo-puppeteer/src/search-refresh.js
+++ csgo-puppeteer/src/search-refresh.js
@@ -15,6 +15,8 @@ let lastList;
 const targetUrl = `https://steamcommunity.com/market/search?q=&category_730_ItemSet%5B%5D=any&category_730_ProPlayer%5B%5D=any&category_730_StickerCapsule%5B%5D=any&category_730_TournamentTeam%5B%5D=any&category_730_Weapon%5B%5D=any&category_730_Exterior%5B%5D=tag_WearCategory0&category_730_Type%5B%5D=tag_Type_Hands&appid=730`;
 let cookieCount = 0;
 let errorCount = 0;
+// 如果物品名称包含任何关键字,就不会被比较和提示.
+const excludeKeywords = ['菱背蛇纹', '风之力-九头蛇弓'];
 
 (async () => {
     // const browser = await puppeteer.launch({ headless: false, delay: 1000 });
@@ -67,7 +69,7 @@ async function extractPage(page) {
             const link = el.href.replace(/(http|https):\/\//, '');
             return { name, count, link };
         }, target);
-        itemList.push(item);
+        isEligibleItem(item) && itemList.push(item);
     };
     console.log(Utils.getLocaleDateTime());
     console.log(itemList.map(item => getItemMsg(item, false)).join('\n'));
@@ -102,4 +104,15 @@ function getItemMsg(item, includeLink) {
     let msg = `名称: ${item.name}, 数量: ${item.count}`;
     includeLink && (msg += `, 链接: <a href="https://${item.link}" target="_blank">点击这里</a>`);
     return msg;
+}
+
+function isEligibleItem(item) {
+    let rs = true;
+    for (let keyword of excludeKeywords) {
+        if (item.name.includes(keyword)) {
+            rs = false;
+            break;
+        }
+    }
+    return rs;
 }
