d12ce6daaf14b2dc2ffcd94324f48e259faf43a0
leon <vcfvct@gmail.com>
2018-05-20 18:00:26 -0400

    add more video options

--- cccg/video-compress.sh
+++ cccg/video-compress.sh
@@ -5,4 +5,6 @@ echo output file: ${d}
 HandBrakeCLI \
   -i /media/lenovo/9016-4EF8/DCIM/393_0520 \
   -o ~/Desktop/${d}.mp4 \
-  -f av_mp4 --ipod-atom -w 420 -l 240 --keep-display-aspect -e x264 -r 30 -q 22.0 -E copy:aac -B 64 -R 22.05 --mixdown stereo 
+  -f av_mp4 -w 418 --loose-anamorphic --modulus 2 --keep-display-aspect \
+  -e x264 -r 30 -q 22.0 --pfr --encoder-profile Baseline --encoder-level 4.0 \
+  -E av_aac -B 64 -R 22.05 --mixdown stereo 
