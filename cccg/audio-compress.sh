f62d1dd6acecf6218d9060767f9afbaa803941c1
Han Li <vcfvct@gmail.com>
2018-05-20 16:25:45 -0400

    adjust quality and add a template

--- cccg/audio-compress.sh
+++ cccg/audio-compress.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 d=`date +%Y%m%d`
 echo output file: ${d}
-
+# vbr(-q): https://trac.ffmpeg.org/wiki/Encode/MP3
 ffmpeg \
   -i /Volumes/sdcard/SOS_DATA/S001/MATERIAL/M000000_.WAV \
-  -ac 1 -ar 16000 \
+  -ac 1 -ar 16000 -q:a 9 \
   -f mp3 ~/Desktop/${d}.mp3 
 
