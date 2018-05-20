23574c1f036a2eed1bce1dc7bb254b823e2d4ebb
Han Li <vcfvct@gmail.com>
2018-05-20 15:36:55 -0400

    break new line

--- cccg/audio-compress.sh
+++ cccg/audio-compress.sh
@@ -2,5 +2,8 @@
 d=`date +%Y%m%d`
 echo output file: ${d}
 
-ffmpeg -i /Volumes/sdcard/SOS_DATA/S001/MATERIAL/M000000_.WAV -ac 1 -ar 16000 -f mp3 ~/Desktop/${d}.mp3 
+ffmpeg \
+  -i /Volumes/sdcard/SOS_DATA/S001/MATERIAL/M000000_.WAV \
+  -ac 1 -ar 16000 \
+  -f mp3 ~/Desktop/${d}.mp3 
 
--- cccg/video-compress.sh
+++ cccg/video-compress.sh
@@ -2,4 +2,7 @@
 d=`date +%Y%m%d`
 echo output file: ${d}
 
-HandBrakeCLI -i /media/lenovo/9016-4EF8/DCIM/393_0520 -o ~/Desktop/${d}.mp4 -f av_mp4 --ipod-atom -w 420 -l 240 --keep-display-aspect -e x264 -r 30 -q 22.0 -E copy:aac -B 64 -R 22.05 --mixdown stereo 
+HandBrakeCLI \
+  -i /media/lenovo/9016-4EF8/DCIM/393_0520 \
+  -o ~/Desktop/${d}.mp4 \
+  -f av_mp4 --ipod-atom -w 420 -l 240 --keep-display-aspect -e x264 -r 30 -q 22.0 -E copy:aac -B 64 -R 22.05 --mixdown stereo 
