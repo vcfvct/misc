fe766beac97a26ac8a979510059052e7da8cb3cf
Han Li <vcfvct@gmail.com>
2018-07-08 12:42:16 -0400

    update tmeplate

--- cccg/audio-compress.sh
+++ cccg/audio-compress.sh
@@ -3,7 +3,7 @@ d=`date +%Y%m%d`
 echo output file: ${d}
 # vbr(-q): https://trac.ffmpeg.org/wiki/Encode/MP3
 ffmpeg \
-  -i /media/lenovo/SDCARD/SOS_DATA/S002/MATERIAL/M000000_.WAV \
+  -i /media/lenovo/SDCARD/SOS_DATA/S004/MATERIAL/M000000_.WAV \
   -ac 1 -ar 16000 -q:a 9 \
   -f mp3 /media/lenovo/TI106412W0C/${d}.mp3 
 
--- cccg/video-compress.sh
+++ cccg/video-compress.sh
@@ -3,7 +3,7 @@ d=`date +%Y%m%d`
 echo output file: ${d}
 
 HandBrakeCLI \
-  -i /media/lenovo/SDCARD/DCIM/394_0527/MVI_0077.MP4 \
+  -i /media/lenovo/SDCARD/DCIM/400_0708/MVI_0608.MP4 \
   -o /media/lenovo/TI106412W0C/${d}.mp4 \
   -f av_mp4 -w 418 --loose-anamorphic --modulus 2 --keep-display-aspect \
   -e x264 -r 30 -q 22.0 --pfr --encoder-profile Baseline --encoder-level 4.0 \
