a1cd3ad0890bcb42c784a431a71b50811476cfd4
Han Li <han.li@capitalone.com>
2018-12-23 20:52:58 -0500

    misc

--- csgo-ts/src/config/item.config.ts
+++ csgo-ts/src/config/item.config.ts
@@ -1,5 +1,5 @@
 export const itemsToScan: Array<ItemToScan> = [
-/*   {
+  {
     nameId: 175967236,
     description: '裹手（★） | 套印 (久经沙场)',
     url: 'https://steamcommunity.com/market/listings/730/%E2%98%85%20Hand%20Wraps%20%7C%20Overprint%20%28Field-Tested%29'
@@ -19,17 +19,17 @@ export const itemsToScan: Array<ItemToScan> = [
     description: 'MP9（StatTrak™） | 气密 (崭新出厂)',
     url: 'https://steamcommunity.com/market/listings/730/StatTrak%E2%84%A2%20MP9%20%7C%20Airlock%20%28Factory%20New%29'
   },
-*/
+
   {
     nameId: 7178002,
     description: 'AK-47 | 红线 (久经沙场)',
     url: 'https://steamcommunity.com/market/listings/730/AK-47%20%7C%20Redline%20%28Field-Tested%29'
   },
-  // {
-  //   nameId: 175854488,
-  //   description: '摩托手套（★） | 日蚀 (久经沙场)',
-  //   url: 'https://steamcommunity.com/market/listings/730/%E2%98%85%20Moto%20Gloves%20%7C%20Eclipse%20%28Field-Tested%29'
-  // }
+  {
+    nameId: 175854488,
+    description: '摩托手套（★） | 日蚀 (久经沙场)',
+    url: 'https://steamcommunity.com/market/listings/730/%E2%98%85%20Moto%20Gloves%20%7C%20Eclipse%20%28Field-Tested%29'
+  }
 ];
 
 export interface ItemToScan {
--- csgo-ts/src/config/runtime.config.ts
+++ csgo-ts/src/config/runtime.config.ts
@@ -1,5 +1,5 @@
 // 扫描间隔, 单位:秒
-export const ScanInterval: number = 4;
+export const ScanInterval: number = 2;
 
 // API超时时间, 单位:秒
 export const ApiTimeout: number = 4;
