a1325098d3b34bc9481008ecc4f0ae530c32bd06
Han Li <han.li@capitalone.com>
2018-12-27 15:03:28 -0500

    add proxy for vpn

--- csgo-ts/src/index.ts
+++ csgo-ts/src/index.ts
@@ -2,10 +2,18 @@ import 'reflect-metadata';
 import Container from 'typedi';
 import { EmailProfileInjectionToken } from './common/constants';
 import { TencentEmail1 } from './config/email.config';
-import { itemsToScan, ItemToScan } from './config/item.config';
-import { ScanInterval } from './config/runtime.config';
-import { ItemOrderHistoryService, ItemOrderHistory } from './service/item-order-history.service';
-import { EmailService } from './service/email.service';
+import { itemsToScan } from './config/item.config';
+import { ItemOrderHistoryService } from './service/item-order-history.service';
+const globalTunnel = require('global-tunnel-ng');
+
+const args = process.argv.slice(2);
+if (args[0] === 'proxy') {
+  console.info(`Setting up global proxy!`);
+  globalTunnel.initialize({
+    host: '127.0.0.1',
+    port: 10800,
+  });
+}
 
 (async () => {
   Container.set(EmailProfileInjectionToken, TencentEmail1);
