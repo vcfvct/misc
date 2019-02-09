ec3fa56aa0e0e37c635e278dd8f609584dde1d61
Han Li <han.li@capitalone.com>
2019-02-09 11:02:18 -0500

    batch ts

---
+++ csgo-ts/src/batch.ts
@@ -0,0 +1,24 @@
+import * as cp from 'child_process';
+import { statSync, readdirSync, readFileSync, writeFileSync } from 'fs';
+import { resolve } from 'path';
+
+const BasePath = '/Users/xex766';
+
+const allDirs = getLevel1Dirs(BasePath);
+allDirs.forEach(dir => {
+  cp.exec(`start cmd.exe /K cd /D ${dir}`);
+});
+
+/**
+ * get level 1 directories back as an Array from a give dir.
+ * @param dir The directory to search from.
+ */
+function getLevel1Dirs(dir: string): string[] {
+  const children: Array<string> = readdirSync(dir);
+  const level1Dirs: string[] = [];
+  children.forEach((child: string) => {
+    const resolvedChild = resolve(dir, child);
+    statSync(resolvedChild).isDirectory() && level1Dirs.push(resolvedChild);
+  });
+  return level1Dirs;
+}
