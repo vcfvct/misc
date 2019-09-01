2a082f44d9041b86f714186d5c4b851af6358a1d
Han Li <vcfvct@gmail.com>
2019-09-01 09:28:59 -0400

    misc

--- cccg/infra-terraform/init.sh
+++ cccg/infra-terraform/init.sh
@@ -14,3 +14,17 @@ sudo add-apt-repository ppa:ondrej/php -y
 sudo apt install php7.0-fpm php7.0-mbstring php7.0-xml php7.0-mysql php7.0-common php7.0-gd php7.0-json php7.0-cli php7.0-curl -y
 sudo systemctl start php7.0-fpm
 systemctl status php7.0-fpm
+
+# Mysql client
+sudo apt-get install mysql-client -y
+# Mysql Server(if host in EC2)
+sudo apt install mysql-server -y
+
+### SSL part [HERE](https://www.digitalocean.com/community/tutorials/how-to-secure-nginx-with-let-s-encrypt-on-ubuntu-16-04)
+sudo openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+# install certbot
+sudo add-apt-repository ppa:certbot/certbot -y
+sudo apt update
+sudo apt install python-certbot-nginx -y
+
+
