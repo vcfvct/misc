850c61c899ba1de34af3ce6e3805b621570f8d96
leon <vcfvct@gmail.com>
2019-10-29 11:18:47 -0400

    Update video-compress.sh

--- cccg/video-compress.sh
+++ cccg/video-compress.sh
@@ -7,6 +7,6 @@ HandBrakeCLI \
   -o ~/Desktop/${d}.mp4 \
   -f av_mp4 -w 418 --loose-anamorphic --modulus 2 --keep-display-aspect \
   -e x264 -r 30 -q 22.0 --pfr --encoder-profile Baseline --encoder-level 4.0 \
-  -E av_aac -B 64 -R 22.05 --mixdown stereo 
+  -E av_aac -B 64 -R 22.05 --mixdown stereo
 
 
