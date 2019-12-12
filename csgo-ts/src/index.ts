b515be8d2cb5e51767ca21dc5c174b02e8497605
Han Li <vcfvct@gmail.com>
2019-12-12 16:32:55 -0500

    add default config id

--- csgo-ts/src/index.ts
+++ csgo-ts/src/index.ts
@@ -7,7 +7,16 @@ const globalTunnel = require('global-tunnel-ng');
 import * as yargs from 'yargs';
 import * as fs from 'fs';
 
-const args = yargs.argv;
+const args = yargs
+  .option('config', {
+    alias: 'c',
+    description: 'specify config file number'
+  })
+  .option('proxy', {
+    alias: 'p',
+    description: 'specify use proxy or not'
+  })
+  .argv;;
 if (args.proxy) {
   console.info(`Setting up global proxy!`);
   globalTunnel.initialize({
@@ -18,8 +27,8 @@ if (args.proxy) {
 
 (async () => {
   Container.set(EmailProfileInjectionToken, TencentEmail1);
-  const configFile = `./src/config/item.config${args.config}.json`;
-  console.info(`Using config file: '${configFile}'`);
+  const configFile = `./src/config/item.config${args.config || 1}.json`;
+  console.info(`Using config file: '${configFile}'. \n`);
   const itemsToScan: Array<ItemToScan> = JSON.parse(fs.readFileSync(configFile, 'utf-8'));
   const itemOrderHistoryService: ItemOrderHistoryService = Container.get(ItemOrderHistoryService);
   itemOrderHistoryService.itemsToScan = itemsToScan.map((i) => {
