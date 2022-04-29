7f6b19dc4b53a60f2c82e95f0352feba7ba3917e
Han Li <vcfvct@gmail.com>
2022-04-28 22:40:42 -0400

    wait for the button to show

--- csgo-tampermonkey/grand-canyon.user.js
+++ csgo-tampermonkey/grand-canyon.user.js
@@ -10,8 +10,11 @@
 // ==/UserScript==
 
 (async () => {
-	const selectButtons = Array.from(document.querySelectorAll('button.mb-8'));
+	const targetButtonSelector = 'button.mb-8';
+	await waitFor(targetButtonSelector);
+	const selectButtons = Array.from(document.querySelectorAll(targetButtonSelector));
 	const disabledButtons = selectButtons.filter(b => b.getAttribute('disabled') === 'disabled');
+	// console.log(selectButtons.length, disabledButtons.length);
 	if (selectButtons.length === disabledButtons.length) {
 		console.log(`Not available, sleep and refresh --- ${new Date().toISOString()}`);
 		setTimeout(() => location.reload(), 5 * 1000);
@@ -34,3 +37,22 @@
 	}
 })();
 
+/**
+ * wait for element to appear in dom.
+ * @returns a Promise which resolves when the element shows up.
+ */
+function waitFor(selector) {
+	return new Promise(function (res, rej) {
+		waitForElementToDisplay(selector, 200);
+		function waitForElementToDisplay(selector, time) {
+			if (document.querySelector(selector) != null) {
+				res(document.querySelector(selector));
+			}
+			else {
+				setTimeout(function () {
+					waitForElementToDisplay(selector, time);
+				}, time);
+			}
+		}
+	});
+}
