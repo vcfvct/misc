344a960c9e2dd7ba567c7a5d35d8babceef6211f
Han Li <vcfvct@gmail.com>
2022-04-28 23:15:37 -0400

    extract the refresh/notification time

--- csgo-tampermonkey/grand-canyon.user.js
+++ csgo-tampermonkey/grand-canyon.user.js
@@ -16,17 +16,19 @@
 	const disabledButtons = selectButtons.filter(b => b.getAttribute('disabled') === 'disabled');
 	// console.log(selectButtons.length, disabledButtons.length);
 	if (selectButtons.length === disabledButtons.length) {
+		const refreshInterval = 5; // 页面刷新间隔
 		console.log(`Not available, sleep and refresh --- ${new Date().toISOString()}`);
-		setTimeout(() => location.reload(), 5 * 1000);
+		setTimeout(() => location.reload(), refreshInterval * 1000);
 	} else {
 		console.log(`stop refreshing and send notification --- ${new Date().toISOString()}`);
 		const params = new Proxy(new URLSearchParams(window.location.search), {
 			get: (searchParams, prop) => searchParams.get(prop),
 		});
+		const notificationTimeout = 10; // notification显示时间
 		const notificationDetails = {
 			text: 'New Room available',
 			title: `Date: ${params.dateFrom}`,
-			timeout: 10000,
+			timeout: notificationTimeout * 1000,
 		};
 		notificationDetails.onclick = () => {
 			parent.focus();
